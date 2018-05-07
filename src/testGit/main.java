a89977d0bb94ae41e7ad7fe4bf88fccdd6179013
ARMAND <ARMAND@10.42.8.164>
2018-05-07 09:22:52 +0200

    Rominou

--- src/testGit/main.java
+++ src/testGit/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Salut kheyou ! ");
+		System.out.println("Salut kheyou Rominou ! ");
 	}
 
 }
